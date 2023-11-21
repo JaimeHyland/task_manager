8727209e89e739ebfb675791edeb6d6e587aaa18
Jaime Hyland <james.hyland@iubh.de>
2023-11-21 10:08:14 +0000

    Dev: add task functionality

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,4 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
   let sidenav = document.querySelectorAll('.sidenav');
   M.Sidenav.init(sidenav);
+
+  let modalDoc = document.querySelectorAll('.modal');
+  M.Modal.init(modalDoc);
+
+  let datepicker = document.querySelectorAll('.datepicker');
+  M.Datepicker.init(datepicker, {
+    format: "dd mmmm, yyyy",
+    i18n: {done: "Select"}
+  });
+
+  let selects = document.querySelectorAll('select');
+  M.FormSelect.init(selects);
 });
