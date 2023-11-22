755c6170a90ecfa37d6b4264fde00e36b50d5794
Jaime Hyland <james.hyland@iubh.de>
2023-11-22 07:54:19 +0000

    Dev: read task functionality

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,16 +1,20 @@
-document.addEventListener('DOMContentLoaded', function() {
-  let sidenav = document.querySelectorAll('.sidenav');
+document.addEventListener("DOMContentLoaded", function() {
+  // sidenav initialization
+  let sidenav = document.querySelectorAll(".sidenav");
   M.Sidenav.init(sidenav);
 
-  let modalDoc = document.querySelectorAll('.modal');
-  M.Modal.init(modalDoc);
-
-  let datepicker = document.querySelectorAll('.datepicker');
+  // datepicker initialization
+  let datepicker = document.querySelectorAll(".datepicker");
   M.Datepicker.init(datepicker, {
-    format: "dd mmmm, yyyy",
-    i18n: {done: "Select"}
+      format: "dd mmmm, yyyy",
+      i18n: {done: "Select"}
   });
 
-  let selects = document.querySelectorAll('select');
+  // select initialization
+  let selects = document.querySelectorAll("select");
   M.FormSelect.init(selects);
+
+  // collapsible initializataion
+  let collapsibles = document.querySelectorAll(".collapsible");
+  M.Collapsible.init(collapsibles);
 });
